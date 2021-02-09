d8609b6c3bf91db1da08c00887b63bc71746e296
Carlito <cchaudron@hotmail.fr>
2021-02-09 13:42:51 +0100

    init tp

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import composant2 from './components/calculatrice.vue'
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 })
+
+
+
+
 </script>
 
 <style>
